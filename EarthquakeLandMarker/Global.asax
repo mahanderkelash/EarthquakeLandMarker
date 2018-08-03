42fa36fbd3d9a26b7e92e19c7fe2a4353b0fb383
Mahander Kelash <mahanderlanghani@gmail.com>
2018-08-03 12:38:49 +0100

    Add project files.

---
+++ EarthquakeLandMarker/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EarthquakeLandMarker.MvcApplication" Language="C#" %>
---
+++ EarthquakeLandMarker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EarthquakeLandMarker.Startup))]
+namespace EarthquakeLandMarker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
