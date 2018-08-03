3c43f7824bbefe383be2b9ebb86b3f057e75d3e2
Mahander Kelash <mahanderlanghani@gmail.com>
2018-08-03 13:53:13 +0100

    EarthquakeCompleteProject

---
+++ EarthquakeLandMarker/Controllers/EarthquakeMapController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EarthquakeLandMarker.Controllers
+{
+    public class EarthquakeMapController : Controller
+    {
+        // GET: EarthquakeMap
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ShowData()
+        {
+            return View();
+        }
+    }
+}
